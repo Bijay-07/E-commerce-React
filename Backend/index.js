5aad677315fea5c22798c1801cb2a650f802964a
Markus <bijayasharma1221@gmail.com>
2026-08-12 19:44:22 +0545

    Vercel Deploy

--- Backend/index.js
+++ Backend/index.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
-const userRoutes = require("./routes/user.route")
-const categoryRoutes = require("./routes/category.route")
-const productRoutes = require("./routes/product.route")
-const cartRoutes = require("./routes/cart.route")
-const orderRoutes = require("./routes/order.route")
-const authRoutes = require("./routes/auth.route");
-const connectDB = require("./config/db")
-const cors = require("cors");
+require("dotenv").config();
 
-const PORT = process.env.PORT;
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const userRoutes = require("./routes/user.route");
+const categoryRoutes = require("./routes/category.route");
+const productRoutes = require("./routes/product.route");
+const cartRoutes = require("./routes/cart.route");
+const orderRoutes = require("./routes/order.route");
+const authRoutes = require("./routes/auth.route");
 
 connectDB();
-app.use(cors());
 
 app.use(express.json());
 
-//Allow only your React frontend
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.REACT_URL,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credential: true
-}));
+    credentials: true,
+  })
+);
 
-//Route to all the models
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
@@ -32,10 +31,8 @@ app.use("/api", cartRoutes);
 app.use("/api", orderRoutes);
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res)=>{
-    res.send("Hello");
-})
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+});
 
-app.listen(PORT, ()=>{
-    console.log("Server is running")
-})
+module.exports = app;
