c495477065758a7dbddccce2df0e807044550879
Markus <bijayasharma1221@gmail.com>
2026-08-12 19:50:13 +0545

    Connect BaseURL in FRONTEND AXIOS

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://e-commerce-react-two-khaki.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
